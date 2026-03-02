ed727405d695a943d1cd10199362652b55cec701
Makarov Egor <egokonmakarov@edu.hse.ru>
2026-03-02 14:42:08 +0300

    redirection logger in infrastructure

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,28 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"context"
-	"log/slog"
 	"os"
 	"os/signal"
 	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/app"
 	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/bot/infrastructure/config"
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/bot/infrastructure/logger"
 )
 
 
 func main() {
 	cfg, err := config.Load();
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to load config: %v\n", err) //не придумал как без нагромождений логгировать
-		os.Exit(1)
+		log.Fatalf("failed to load config: %v\n", err) //не придумал как без нагромождений логгировать
 	}
 
-	level := config.ParseLogLevel(cfg.LogLevel)
-
-	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-		Level: level,
-	}))
+	logger := logger.New(cfg.LogLevel)
 
 	App := app.New(cfg, logger)
 
---
+++ internal/bot/infrastructure/logger/logger.go
@@ -0,0 +1,16 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/internal/bot/infrastructure/config"
+)
+
+
+func New(log_lvl string) (logger *slog.Logger) {
+	level := config.ParseLogLevel(log_lvl)
+	logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: level,
+	}))
+	return logger
+}
